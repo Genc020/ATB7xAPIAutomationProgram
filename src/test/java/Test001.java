00f37c6ed8b4ebba2402d5506b245c1fbfaf9210
Gen020 <acpithale@mitaoe.ac.in>
2024-11-29 17:31:56 +0530

    Test created

--- src/test/java/Test001.java
+++ src/test/java/Test001.java
@@ -1,2 +1,14 @@
+import io.restassured.RestAssured;
+
 public class Test001 {
+
+    public static void main (String[] args){
+
+        RestAssured.given().baseUri("https://restful-booker.herokuapp.com")
+                .basePath("/booking").log().all()
+                .when()
+                .get()
+                .then().log().all()
+                .statusCode(200);
+    }
 }
